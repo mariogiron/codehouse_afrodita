5efb60344b4b38c3f410c03beb04a7eff2952bf3
Mario Girón <mariogironm@gmail.com>
2021-07-15 11:02:42 +0200

    Select

--- express/AfroditaShop/models/product.model.js
+++ express/AfroditaShop/models/product.model.js
@@ -9,6 +9,22 @@ const getAll = () => {
     return prom;
 };
 
+// Recupero un producto por ID
+// select * from products where id = 1
+const getById = (productId) => {
+    return new Promise((resolve, reject) => {
+        db.query(
+            'select * from products where id = ?',
+            [productId],
+            (err, result) => {
+                if (err) return reject(err);
+                if (result.length !== 1) return resolve(null);
+                resolve(result[0]);
+            }
+        );
+    });
+};
+
 module.exports = {
-    getAll
+    getAll, getById
 }
--- express/AfroditaShop/routes/api/products.js
+++ express/AfroditaShop/routes/api/products.js
@@ -1,20 +1,51 @@
 const router = require('express').Router();
 
-// TODO: Extraer solo las funciones que vamos a usar (destructuring)
-const productModel = require('../../models/product.model');
+const { getAll, getById } = require('../../models/product.model');
 
 router.get('/', (req, res) => {
     // 1 - Recuperar los productos de la base de datos
-    // TODO: opción async-await
-    productModel.getAll()
-        .then(result => {
-            res.json(result);
-        })
-        .catch(err => {
-            res.json(err.message);
-        });
     // 2 - Enviar en formato JSON los productos al cliente
+    getAll()
+        .then(result => res.json(result))
+        .catch(err => res.json({ error: err.message }));
 });
+
+router.get('/v2', async (req, res) => {
+    try {
+        const result = await getAll();
+        res.json(result);
+    } catch (err) {
+        res.json({ error: err.message });
+    }
+});
+
+router.get('/:productId', async (req, res) => {
+    try {
+        const result = await getById(req.params.productId);
+        if (result) {
+            res.json(result);
+        } else {
+            res.json({ error: 'El producto no existe en la base de datos' });
+        }
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+router.get('/:productId/v2', (req, res) => {
+    getById(req.params.productId)
+        .then(result => {
+            if (result) {
+                res.json(result);
+            } else {
+                res.json({ error: 'El producto no existe en la base de datos' });
+            }
+        })
+        .catch(error => res.json({ error: error.message }));
+});
+
+
+
 
 
 
